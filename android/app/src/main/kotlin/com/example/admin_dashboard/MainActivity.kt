f182160d5f041548a0c7996cd6b1615476674062
bukunmialuko <oluwabukunmi.aluko@gmail.com>
2026-05-15 14:58:17 +0100

    build(android): upgrade to Gradle 8.10, AGP 8.7, Kotlin 1.9, Java 17

--- android/app/src/main/kotlin/com/example/admin_dashboard/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.admin_dashboard
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/ng/com/obkm/admin_dashboard/MainActivity.kt
@@ -0,0 +1,5 @@
+package ng.com.obkm.admin_dashboard
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
