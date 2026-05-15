29ec65606eaaae3cec2177ec4843e27f68600aae
bukunmialuko <oluwabukunmi.aluko@gmail.com>
2026-05-15 15:16:31 +0100

    refactor(web): migrate to flutter_bootstrap.js initialization

---
+++ web/flutter_bootstrap.js
@@ -0,0 +1,16 @@
+{{flutter_js}}
+{{flutter_build_config}}
+
+_flutter.loader.load({
+  onEntrypointLoaded: async function (engineInitializer) {
+    const appRunner = await engineInitializer.initializeEngine();
+    setTimeout(() => {
+      const preloader = document.querySelector('.preloader');
+      if (preloader) {
+        preloader.style.opacity = '0';
+        preloader.style.display = 'none';
+      }
+    }, 500);
+    await appRunner.runApp();
+  },
+});
